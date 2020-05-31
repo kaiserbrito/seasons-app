87e67622bbddef293ebcc3ae8889dfdfeb07c594
Victor Guerra <victorbcguerra@gmail.com>
2020-05-31 17:27:13 +0100

    Imported Spinner when waiting user action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -18,6 +19,7 @@ class App extends React.Component {
 
   render() {
     return <SeasonDisplay latitude={this.state.lat} error={this.state.errorMessage} />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
