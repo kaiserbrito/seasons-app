30c2b6eba9b9a12a831e76804b3eeae9ffca0e50
Victor Guerra <victorbcguerra@gmail.com>
2020-05-31 17:28:02 +0100

    Refactored component to display dynamic messages depend on location

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,22 +1,38 @@
 import "./SeasonDisplay.css";
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's go the Beach",
+    iconName: "sun",
+  },
+  winter: {
+    text: "It's chilling",
+    iconName: "snowflake",
+  },
+};
+
+const getSeason = (lat, mounth) => {
+  if (mounth > 2 && mounth < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = (props) => {
-  let returnDiv = null;
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
 
-  if (props.error && !props.latitude) {
-    returnDiv = <div>Error: {props.error}</div>;
-    return returnDiv;
-  }
-
-  if (!props.error && props.latitude) {
-    returnDiv = <div>Latitude: {props.latitude}</div>;
-    return returnDiv;
-  }
-
-  returnDiv = <div>Loading...</div>;
-
-  return returnDiv;
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>
+        <div>{text}</div>
+      </h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
